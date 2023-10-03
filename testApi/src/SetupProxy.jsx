a691fa1d7845a11f7b64b1d1c2bafb8cf25c60b3
Furkan Şahin <fsahin5834@gmail.com>
2023-10-03 22:36:34 +0300

    Created setupProxy

---
+++ testApi/src/SetupProxy.jsx
@@ -0,0 +1,14 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+    
+// module.exports = function(app) {
+
+//     app.use(
+//     '/getDetails', //this is your api
+//     createProxyMiddleware({
+//       target:'http://10.0.0.20:9000/getDetails', //this is your whole endpoint link
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
+
